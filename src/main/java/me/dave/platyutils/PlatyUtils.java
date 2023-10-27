4becbb003826f592caeef6d53ecceb7c399257f9
BuildTools <cooldaveb@live.co.uk>
2023-10-27 16:05:17 +0100

    Added Updater

--- src/main/java/me/dave/platyutils/PlatyUtils.java
+++ src/main/java/me/dave/platyutils/PlatyUtils.java
@@ -1,9 +1,13 @@
 package me.dave.platyutils;
 
+import me.dave.platyutils.listener.InventoryListener;
+import me.dave.platyutils.listener.PlayerListener;
 import me.dave.platyutils.manager.GuiManager;
 import me.dave.platyutils.manager.Manager;
+import org.bukkit.event.Listener;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.jetbrains.annotations.NotNull;
+import space.arim.morepaperlib.MorePaperLib;
 
 import java.util.HashMap;
 import java.util.Optional;
@@ -11,14 +15,27 @@ import java.util.logging.Logger;
 
 public final class PlatyUtils {
     private static boolean enabled = false;
+
+    private static JavaPlugin plugin = null;
     private static Logger logger = null;
+    private static MorePaperLib morePaperLib = null;
+
     private static HashMap<Class<? extends Manager>, Manager> managers;
 
-    public static void enable(@NotNull JavaPlugin plugin) {
+    public void enable(@NotNull JavaPlugin plugin) {
         enabled = true;
+
+        PlatyUtils.plugin = plugin;
         logger = plugin.getLogger();
 
         registerManager(new GuiManager());
+
+        registerEvents(
+            new InventoryListener(),
+            new PlayerListener()
+        );
+
+        logger.info("Successfully enabled PlatyUtils");
     }
 
     public static void disable() {
@@ -28,15 +45,46 @@ public final class PlatyUtils {
             managers = null;
         }
 
+        if (morePaperLib != null) {
+            morePaperLib.scheduling().cancelGlobalTasks();
+            morePaperLib = null;
+        }
+
+        logger.info("Successfully disabled PlatyUtils");
+        logger = null;
+        plugin = null;
+
         enabled = false;
+    }
+
+    private void registerEvents(Listener... listeners) {
+        for (Listener listener : listeners) {
+            plugin.getServer().getPluginManager().registerEvents(listener, plugin);
+        }
     }
 
     public static boolean isEnabled() {
         return enabled;
     }
 
+    public static JavaPlugin getPlugin() {
+        return plugin;
+    }
+
     public static Logger getLogger() {
         return logger;
+    }
+
+    public static MorePaperLib getMorePaperLib() {
+        if (isEnabled()) {
+            if (morePaperLib == null) {
+                morePaperLib = new MorePaperLib(plugin);
+            }
+
+            return morePaperLib;
+        } else {
+            throw new IllegalStateException("PlatyUtils is not enabled");
+        }
     }
 
     @SuppressWarnings("unchecked")
