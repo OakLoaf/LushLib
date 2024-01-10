46e6bc34f80365e098c5393a4a44e6a8844abea9
BuildTools <cooldaveb@live.co.uk>
2024-01-10 16:28:26 +0000

    Added error handling to enable and disable methods

--- src/main/java/me/dave/platyutils/module/Module.java
+++ src/main/java/me/dave/platyutils/module/Module.java
@@ -1,7 +1,11 @@
 package me.dave.platyutils.module;
 
+import me.dave.platyutils.PlatyUtils;
 import me.dave.platyutils.listener.EventListener;
 
+import java.util.logging.Level;
+
+@SuppressWarnings("unused")
 public abstract class Module {
     private final String id;
     private boolean enabled = false;
@@ -21,20 +25,38 @@ public abstract class Module {
     protected void onEnable() {}
 
     public final void enable() {
-        this.onEnable();
+        try {
+            this.onEnable();
+        } catch (Throwable e) {
+            PlatyUtils.getPlugin().log(Level.SEVERE, "Error when enabling module '" + id + "' at:");
+            e.printStackTrace();
+            PlatyUtils.getPlugin().log(Level.SEVERE, "Disabling module '" + id + "'");
+            disable();
+            return;
+        }
+
         if (this instanceof EventListener listener) {
             listener.registerListeners();
         }
+
         this.enabled = true;
+        PlatyUtils.getPlugin().log(Level.INFO, "Successfully enabled module '" + id + "'");
     }
 
     protected void onDisable() {}
 
     public final void disable() {
-        this.onDisable();
+        try {
+            this.onDisable();
+        } catch (Throwable e) {
+            PlatyUtils.getPlugin().log(Level.SEVERE, "Error when disabling module '" + id + "' at:");
+            e.printStackTrace();
+        }
+
         if (this instanceof EventListener listener) {
             listener.unregisterListeners();
         }
+
         this.enabled = false;
     }
 
