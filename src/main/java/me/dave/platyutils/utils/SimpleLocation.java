783f60eca9eef9f8316554417f61ddfc62afd8f3
BuildTools <cooldaveb@live.co.uk>
2024-01-16 18:39:37 +0000

    Added World to SimpleLocation

--- src/main/java/me/dave/platyutils/utils/SimpleLocation.java
+++ src/main/java/me/dave/platyutils/utils/SimpleLocation.java
@@ -1,19 +1,22 @@
 package me.dave.platyutils.utils;
 
 import org.bukkit.Location;
+import org.bukkit.World;
+
+import java.util.Objects;
 
 @SuppressWarnings("unused")
-public record SimpleLocation(double x, double y, double z) {
+public record SimpleLocation(World world, double x, double y, double z) {
 
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
         SimpleLocation that = (SimpleLocation) o;
-        return Double.compare(that.x, x) == 0 && Double.compare(that.y, y) == 0 && Double.compare(that.z, z) == 0;
+        return Double.compare(that.x, x) == 0 && Double.compare(that.y, y) == 0 && Double.compare(that.z, z) == 0 && Objects.equals(world, that.world);
     }
 
-    public static SimpleLocation from(Location location) {
-        return new SimpleLocation(location.getX(), location.getY(), location.getZ());
+    public static SimpleLocation adapt(Location location) {
+        return new SimpleLocation(location.getWorld(), location.getX(), location.getY(), location.getZ());
     }
 }
