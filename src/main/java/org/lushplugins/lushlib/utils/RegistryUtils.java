eecc37a2b530ea04c4f16aee171ef22d492e4d11
Oak <cooldaveb@live.co.uk>
2024-05-22 17:06:21 +0100

    Added fromStringList method

--- src/main/java/org/lushplugins/lushlib/utils/RegistryUtils.java
+++ src/main/java/org/lushplugins/lushlib/utils/RegistryUtils.java
@@ -4,7 +4,14 @@ import org.bukkit.Keyed;
 import org.bukkit.NamespacedKey;
 import org.bukkit.Registry;
 
+import java.util.Collection;
+import java.util.List;
+
 public class RegistryUtils {
+
+    public static <T extends Keyed> List<T> fromStringList(Registry<T> registry, Collection<String> stringList) {
+        return stringList.stream().map(string -> RegistryUtils.fromString(registry, string)).toList();
+    }
 
     public static <T extends Keyed> T fromString(Registry<T> registry, String string) {
         NamespacedKey namespacedKey = NamespacedKey.fromString(string);
