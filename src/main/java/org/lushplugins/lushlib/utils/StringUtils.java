d4303808ae97c1e250463c092c3be4ef04673297
OakLoaf <oak@beaconstudios.org>
2025-01-28 17:17:53 +0000

    Added makeFriendly method to StringUtils

--- src/main/java/org/lushplugins/lushlib/utils/StringUtils.java
+++ src/main/java/org/lushplugins/lushlib/utils/StringUtils.java
@@ -7,6 +7,22 @@ import java.util.Optional;
 @SuppressWarnings("unused")
 public class StringUtils {
 
+    public static String makeFriendly(String string) {
+        StringBuilder output = new StringBuilder();
+
+        String[] words = string.toLowerCase().split(" ");
+        for (int i = 0; i < words.length; i++) {
+            if (i > 0) {
+                output.append(" ");
+            }
+
+            String word = words[i];
+            output.append(word.substring(0, 1).toUpperCase()).append(word.substring(1));
+        }
+
+        return output.toString();
+    }
+
     public static <T extends Enum<T>> Optional<T> getEnum(String content, Class<T> clazz) {
         return getEnum(content, clazz, null);
     }
