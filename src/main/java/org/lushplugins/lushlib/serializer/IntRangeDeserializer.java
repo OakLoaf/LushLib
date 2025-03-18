f38d3cec1e613dd2a6748058df7254532f5d86cb
OakLoaf <oak@beaconstudios.org>
2025-03-18 16:51:35 +0000

    Updated deserializer

--- src/main/java/org/lushplugins/lushlib/serializer/IntRangeDeserializer.java
+++ src/main/java/org/lushplugins/lushlib/serializer/IntRangeDeserializer.java
@@ -1,35 +1,11 @@
 package org.lushplugins.lushlib.serializer;
 
-import com.fasterxml.jackson.core.JsonLocation;
-import com.fasterxml.jackson.core.JsonParser;
-import com.fasterxml.jackson.databind.DeserializationContext;
-import com.fasterxml.jackson.databind.JsonDeserializer;
 import org.lushplugins.lushlib.utils.IntRange;
-import org.lushplugins.lushlib.LushLogger;
 
-import java.io.IOException;
-
-public class IntRangeDeserializer extends JsonDeserializer<IntRange> {
+public class IntRangeDeserializer extends TextDeserializer<IntRange> {
 
     @Override
-    public IntRange deserialize(JsonParser jsonParser, DeserializationContext deserializationContext) throws IOException {
-        Object obj = jsonParser.currentValue();
-
-        IntRange deserialized;
-        try {
-            deserialized = IntRange.valueOf(obj);
-        } catch (NumberFormatException e) {
-            deserialized = null;
-
-            JsonLocation location = jsonParser.currentLocation();
-            LushLogger.getLogger().severe("%s cannot deserialize %s at: %s:%s".formatted(
-                getClass().getSimpleName().toLowerCase(),
-                obj,
-                location.getLineNr(),
-                location.getColumnNr()
-            ));
-        }
-
-        return deserialized;
+    public IntRange deserialize(String text) {
+        return IntRange.parseIntRange(text);
     }
 }
