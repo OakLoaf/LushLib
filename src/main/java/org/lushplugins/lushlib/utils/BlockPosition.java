e560f501aa7095fb343f0126d2e0623ae623693b
OakLoaf <oak@beaconstudios.org>
2025-03-19 18:03:22 +0000

    Added useful methods to BlockPosition

--- src/main/java/org/lushplugins/lushlib/utils/BlockPosition.java
+++ src/main/java/org/lushplugins/lushlib/utils/BlockPosition.java
@@ -4,11 +4,25 @@ import org.bukkit.Location;
 import org.bukkit.World;
 import org.bukkit.block.Block;
 
+import java.util.Objects;
+
 @SuppressWarnings("unused")
 public record BlockPosition(World world, int x, int y, int z) {
 
     public Block getBlock() {
         return world.getBlockAt(x, y, z);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        BlockPosition that = (BlockPosition) o;
+        return Double.compare(that.x, x) == 0 && Double.compare(that.y, y) == 0 && Double.compare(that.z, z) == 0 && Objects.equals(world, that.world);
+    }
+
+    public static BlockPosition from(Block block) {
+        return new BlockPosition(block.getWorld(), block.getX(), block.getY(), block.getZ());
     }
 
     public static BlockPosition adapt(Location location) {
