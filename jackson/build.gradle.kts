7b5b2cd8d542bb6ea0e99cb03b9a8429e527164a
OakLoaf <oak@beaconstudios.org>
2026-03-14 22:55:47 +0000

    Adjusted scope of jackson dependencies

--- jackson/build.gradle.kts
+++ jackson/build.gradle.kts
@@ -2,8 +2,8 @@ dependencies {
     compileOnly(project(":common"))
     compileOnly(project(":utils"))
 
-    implementation("com.fasterxml.jackson.core:jackson-core:2.20.0") // Jackson
-    implementation("com.fasterxml.jackson.core:jackson-annotations:2.20") // Jackson
-    implementation("com.fasterxml.jackson.dataformat:jackson-dataformat-yaml:2.20.0") // Jackson
-    implementation("com.fasterxml.jackson.datatype:jackson-datatype-jsr310:2.20.0") // Jackson (DataTypes)
+    api("com.fasterxml.jackson.core:jackson-core:2.20.0") // Jackson
+    api("com.fasterxml.jackson.core:jackson-annotations:2.20") // Jackson
+    api("com.fasterxml.jackson.dataformat:jackson-dataformat-yaml:2.20.0") // Jackson
+    api("com.fasterxml.jackson.datatype:jackson-datatype-jsr310:2.20.0") // Jackson (DataTypes)
 }
